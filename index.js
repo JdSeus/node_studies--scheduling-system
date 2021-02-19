b305c53a4d1f1ba238a562ae8fd7450268f57226
Juliano Seus <JulianoSeusDev@outlook.com>
2021-02-19 08:37:26 -0300

    setup do projeto

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+
+//Configs
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.set('view engine', 'ejs');
+
+mongoose.connect("mongodb://localhost:27017/agendamento", {useNewUrlParser: true, useUnifiedTopology: true});
+//END Configs
+
+
+//Routes
+app.get("/", (req, res) => {
+    res.send("Oi!");
+});
+//END Routes
+
+
+app.listen(8080, () => {});
